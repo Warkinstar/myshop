f7335caa768f3b5dcf6cdfa059ef764d2d308d64
Ramil <Warkinstar@mail.ru>
2023-06-19 22:56:19 +0600

    Adding coupons to orders on admin site and pdf invoices

--- payment/views.py
+++ payment/views.py
@@ -40,6 +40,21 @@ def payment_process(request):
                     "quantity": item.quantity,
                 }
             )
+        # Stripe coupon
+        if order.coupon:
+            # Creation stripe coupon object
+            stripe_coupon = stripe.Coupon.create(
+                name=order.coupon.code,
+                percent_off=order.discount,
+                duration="once",
+            )
+            # Coupon id is added to the session_data Stripe Checkout session
+            session_data["discounts"] = [
+                {
+                    "coupon": stripe_coupon.id,
+                }
+            ]
+
         # create Stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
         # redirect to Stripe payment form
